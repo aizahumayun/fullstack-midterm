8e85209ab0d5ec3da01aefffb4124fde1f500d41
Aiza Humayun <123739993+aizahumayun@users.noreply.github.com>
2025-12-22 12:35:19 +0500

    changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/quotes",
+  baseURL: import.meta.env.QUOTES_API_BASE_URL || "http://localhost:3000/api/quotes",
   headers: {"content-type": "application/json"},
 });
 
